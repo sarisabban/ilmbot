a18708938e665192927a5264bf132338ae301b13
acresearch <acresearch@acresearch>
2017-04-27 12:08:21 +0300

    Changed the entire bot to make it use BeautifulSoup as a parser and ignore unsupported terminal charachters.

--- ilm.py
+++ ilm.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-	  # <--- install module with pip3
-import tweepy , urllib.request , re , random , datetime , time
+	  # 	 # <--- install module with pip3
+import tweepy , bs4 , urllib.request , re , random , datetime , time
 
 #Tuples of Websites and their Regular Expressions:
-Website1 = ('http://www.scientificsaudi.com/main/articles' , 'href="(http://www.scientificsaudi.com/ss/[0-9]*?)">')
-Website2 = ('https://syr-res.com' , 'href="(https://syr-res.com/article/[0-9].*?.html)')
-Website3 = ('http://n-scientific.org' , 'href="(http://n-scientific.org/[0-9].*?)"\s.*?</h3>')
+Website1 = ('http://www.scientificsaudi.com/main/articles' , 'http://www.scientificsaudi.com/ss/[0-9].*')
+Website2 = ('https://syr-res.com' , 'https://syr-res.com/article/[0-9].*?.html')
+Website3 = ('http://n-scientific.org' , 'http://n-scientific.org/[0-9].*#')
 
 #Lists of Tuples: Websites and their Regular Expressions
 WEBlist	= [Website1 , Website2 , Website3]
@@ -33,30 +33,37 @@ api = tweepy.API(auth)
 print('\x1b[33m' + '[+] Connected To Twitter' + '\x1b[0m')
 print('\x1b[35m' + '------------------------------' + '\x1b[0m')
 #-----------------------------------------------------------------------------------------------------------
-def GetLink (Website , RegularExpression):
+def GetLink(TheWeb , TheRE):
 	'''Parses a website, gets all links that satisfy the given regular expression with their titles, then returns a random tuple with link and the page's title'''
-	web = urllib.request.urlopen(Website)
-	URLs = list()
-	for line in web:
-		line = line.decode()
-		article = re.findall(RegularExpression , line)
-		if article == []:
-			continue
-		else:
-			URLs.append(article[0])
-	random.shuffle(URLs)
-	URL=URLs[0]
+	while True:
+		try:
+			web = urllib.request.urlopen(TheWeb)
+			data = bs4.BeautifulSoup(web , 'html5lib')
 
-	web = urllib.request.urlopen(URLs[0])
-	for line in web:
-		line = line.decode()
-		article = re.findall('<title>(.*?)</title>' , line)
-		if article == []:
-			continue
-		else:
-			TITLE = article[0]
-	return(URL , TITLE)
+			URLs = set()
+			for link in data.find_all('a'):
+				ArticleLinks = link.get('href')
+				if re.search(TheRE , str(ArticleLinks)):
+					URLs.add(ArticleLinks) 
 
+			URL=URLs.pop()
+
+			article = urllib.request.urlopen(URL)
+			articledata=bs4.BeautifulSoup(article , 'html5lib')
+			Title = articledata.title.text
+			if re.search('Ř' , Title):
+				print('\x1b[31m' + '[-] Error')
+				print('Problem with page title encoding' + '\x1b[0m')
+				continue
+			else:
+				
+				return(Title , URL)
+				break
+
+		except Exception as TheError:
+			print('\x1b[31m' + '[-] ERROR in GetLink function')
+			print(TheError)
+			print('\x1b[0m')
 
 
 def Tweet (TheWeb , TheRE):
@@ -64,13 +71,14 @@ def Tweet (TheWeb , TheRE):
 	try:
 		dateSTR = datetime.datetime.now().strftime('%H:%M')
 		Result = GetLink(TheWeb , TheRE)
-		TheTweet = Result[1] + '\n' + Result[0] + '\n' + '#علوم'
+		TheTweet = Result[0] + '\n' + Result[1] + '\n' + '#علوم'
 		api.update_status(TheTweet)
 		print('\x1b[34m' + '[+] Tweet at ' + dateSTR + '\x1b[0m' + '\n' + TheTweet + '\n')
 
 	except Exception as TheError:
-		print('\x1b[31m' + '[-] ERROR' + '\x1b[0m')
+		print('\x1b[31m' + '[-] ERROR in Tweet function')
 		print(TheError)
+		print('\x1b[0m')
 #-----------------------------------------------------------------------------------------------------------
 random.shuffle(WEBlist)
 TheWebsite = WEBlist[0]
