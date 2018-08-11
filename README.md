9d9d0087bfddbaf062b1755849a0675001ae9e76
acresearch <sari.sabban@gmail.com>
2018-08-11 15:51:43 +0300

    small updates

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # ilmbot
+A Twitter bot for the twitter account of my podcast ILM FM:
+
+## Website:
+http://ILM.FM
 
 ## Description:
-This is a twitter bot specific for the Ilm FM science podcast's twitter account. The bot parses some science news websites in order to search for science articles. It then tweets a random link from the websites.
+This is a twitter bot specific for the ILM FM science podcast's twitter account. The bot parses some science news websites in order to search for science articles. It then tweets a random link from the websites. The bot is designed to work on Debian based linux destributions.
 
 ## How to Use:
-
 1. Install Dependecies
 
-```
-pip3 install tweepy bs4 html5lib
-```
+`sudo apt update && sudo apt full-upgrade && sudo apt install python-beautifulsoup python-lxml python-tweepy`
 
 2. Add the Twitter credentials to the keys.py file.
-3. You can excute the script manually `python3 ilm.py`. But a more efficient way is to add the command as a cronjob.
+3. You can excute the script manually `python ilm.py`. But a more efficient way is to setup a crons job (times are in UTC):
 
+`crontab -e`
+
+`
+32 17 * * * python3 ilm.py >> ilm_Log 2>&1
+12 18 * * * python3 ilm.py >> ilm_Log 2>&1
+03 19 * * * python3 ilm.py >> ilm_Log 2>&1
+08 20 * * * python3 ilm.py >> ilm_Log 2>&1
+`
 
 ## Configuration
-
-To add a new website make a new variable containing a tuple under "Lists of Tuples" with the website's URL in the first position and the best way to identify article URLs using Regular Expression in the second position.
-
-Next add your website tuple to the WEBlist under "Lists of Tuples".
+1. To add a new website make a new variable containing a tuple under "#Tuples of websites and their regular expressions" with the website's URL in the first position and the best way to identify article URLs using regular expression in the second position.
+2. Next add your website's tuple to the WEBlist list under "Lists of tuples".
--- ilm.py
+++ ilm.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-import tweepy , bs4 , urllib.request, urllib.parse , re , random , datetime , time
+import urllib.request
+import urllib.parse
+import datetime
+import random
+import tweepy
+import time
+import bs4
+import re
 
-#Tuples Of Websites And Their Regular Expressions
-Website1 = ('http://www.scientificsaudi.com/main/articles' , 'http://www.scientificsaudi.com/ss/[0-9].*')
-Website2 = ('https://syr-res.com' , 'https://syr-res.com/article/[0-9].*?.html')
-Website3 = ('http://n-scientific.org' , 'http://n-scientific.org/[0-9].*#')
-Website4 = ('https://oloom.news' , 'https://oloom\.news/%.*')
-Website5 = ('http://www.bbc.com/arabic/scienceandtech' , '/arabic/science-and-tech-[0-9].*')
-Website6 = ('http://www.reuters.com/news/science' , '/article/')
+#Tuples of websites and their regular expressions
+Website1 = ('http://www.scientificsaudi.com/main/articles', 'http://www.scientificsaudi.com/ss/[0-9].*')
+Website2 = ('https://syr-res.com', 'https://syr-res.com/article/[0-9].*?.html')
+Website3 = ('http://n-scientific.org', 'http://n-scientific.org/[0-9].*#')
+Website4 = ('https://oloom.news', 'https://oloom\.news/%.*')
+Website5 = ('http://www.bbc.com/arabic/scienceandtech', '/arabic/science-and-tech-[0-9].*')
+Website6 = ('http://www.reuters.com/news/science', '/article/')
 
-#Lists of Tuples: Websites And Their Regular Expressions
-WEBlist	= [Website1 , Website2 , Website3 , Website4 , Website5 , Website6] 
+#Lists of tuples: websites and their regular expressions
+WEBlist = [Website1, Website2, Website3, Website4, Website5, Website6]
 
-#Twitter Credentials
-from keys import *
+#Twitter credentials
+consumer_key		= ''
+consumer_secret		= ''
+access_token		= ''
+access_token_secret	= ''
 
-#Terminal Text Colours
+#Terminal text colours
 Black 	= '\x1b[30m'
 Red	= '\x1b[31m'
 Green	= '\x1b[32m'
@@ -26,46 +36,60 @@ Cyan	= '\x1b[36m'
 White	= '\x1b[37m'
 Cancel	= '\x1b[0m'
 
-#Connecting To Twitter
-auth = tweepy.OAuthHandler(consumer_key , consumer_secret)
-auth.set_access_token(access_token , access_token_secret)
+#Connecting to twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-#-----------------------------------------------------------------------------------------------------------
-#Functions
-def GetLink(TheWeb , TheRE):
-	''' Parses a website, gets all links that satisfy the given regular expression with their titles, then returns a random tuple with a URL link and the page's title '''
+
+def GetLink(TheWeb, TheRE):
+	'''
+	Parses a website, gets all links that satisfy the given regular
+	expression with their titles, then returns a random tuple with
+	a URL link and the page's title
+	'''
 	web = urllib.request.urlopen(TheWeb)
-	data = bs4.BeautifulSoup(web , 'lxml')
+	data = bs4.BeautifulSoup(web, 'lxml')
 	URLs = set()
 	for link in data.find_all('a'):
 		ArticleLinks = link.get('href')
-		if re.search(TheRE , str(ArticleLinks)):
+		if re.search(TheRE, str(ArticleLinks)):
 			URLs.add(urllib.parse.urljoin(TheWeb, ArticleLinks))
 	URL = URLs.pop()
 	article = urllib.request.urlopen(URL)
-	articledata = bs4.BeautifulSoup(article , 'lxml')
+	articledata = bs4.BeautifulSoup(article, 'lxml')
 	Title = articledata.title.text
-	if re.search('–' , Title):
-		Title = Title.replace('–' , '-')
-	if re.search('Ř' , Title):
+	if re.search('–', Title):
+		Title = Title.replace('–', '-')
+	if re.search('Ř', Title):
 		raise Exception('incorrect title encoding')
 	else:	
-		return(Title , URL)
+		return(Title, URL)
 
-def Tweet (TheWeb , TheRE):
-	''' Tweets a weblink and its title '''
-	Result = GetLink(TheWeb , TheRE)
-	TheTweet = Result[0] + '\n' + Result[1] + '\n' + '#علوم'
+def Tweet(TheWeb, TheRE):
+	'''
+	Tweets a weblink and its title
+	'''
+	Result = GetLink(TheWeb, TheRE)
+	TheTweet = '{}\n{}\n#علوم'.format(Result[0], Result[1])
 	api.update_status(TheTweet)
-#-----------------------------------------------------------------------------------------------------------
-for attempts in range(1000):
-	random.shuffle(WEBlist)
-	TheWebsite = WEBlist[0]
-	dateSTR = datetime.datetime.now().strftime('%d %B %Y @ %H:%M')
-	try:
-		Tweet(TheWebsite[0] , TheWebsite[1])
-		print(Green + '[+] Tweeted -' , dateSTR + Cancel , TheWebsite[0])
-		break
-	except Exception as TheError:
-		print(Red + '[-]' , str(TheError) , dateSTR + Cancel , TheWebsite[0])
-		continue
+
+def main():
+	'''
+	Run the script with a loop to by pass errors
+	'''
+	for attempts in range(1000):
+		random.shuffle(WEBlist)
+		TheWebsite = WEBlist[0]
+		dateSTR = datetime.datetime.now().strftime('%d %B %Y @ %H:%M')
+		try:
+			Tweet(TheWebsite[0], TheWebsite[1])
+			print('{}[+] Tweeted -{}{}{}'.format(Green,
+				dateSTR, Cancel, TheWebsite[0]))
+			break
+		except Exception as TheError:
+			print('{}[-]{}{}{}{}'.format(Red,
+				str(TheError), dateSTR, Cancel, TheWebsite[0]))
+			continue
+
+if __name__ == '__main__':
+	main()
