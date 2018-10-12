3646177bafb44c6925d8879767ad2cdc6afe7ad9
sarisabban <sari.sabban@gmail.com>
2018-10-12 07:49:41 +0300

    Majore update to the bot that restructured its functions and added RSS feed tweeting

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ A Twitter bot for the twitter account of my podcast ILM FM:
 http://ILM.FM
 
 ## Description:
-This is a twitter bot specific for the ILM FM science podcast's twitter account. The bot parses some science news websites in order to search for science articles. It then tweets a random link from the websites. The bot is designed to work on Debian based linux destributions.
+This is a twitter bot specific for the ILM FM science podcast's twitter account. It has the following functions:
+* Parses science news websites searching for science articles and tweets a random article.
+* Tweets an annoucement when an episode is published, uses RSS therefore you can keep the script on repeat and will not tweet as long as the RSS has not been updated.
 
 ## How to Use:
 1. Install Dependecies
 
-`sudo apt update && sudo apt full-upgrade && sudo apt install python-beautifulsoup python-lxml python-tweepy`
+`sudo apt update && sudo apt full-upgrade && sudo apt install python-beautifulsoup python-lxml python-twython`
 
 2. Add the Twitter credentials to the keys.py file.
 3. You can excute the script manually `python ilm.py`. But a more efficient way is to setup a crons job (times are in UTC):
@@ -18,12 +20,14 @@ This is a twitter bot specific for the ILM FM science podcast's twitter account.
 `crontab -e`
 
 `
-32 17 * * * python3 ilm.py >> ilm_Log 2>&1
-12 18 * * * python3 ilm.py >> ilm_Log 2>&1
-03 19 * * * python3 ilm.py >> ilm_Log 2>&1
-08 20 * * * python3 ilm.py >> ilm_Log 2>&1
+00 00 * * * python3 ilm.py rss >> ilm_Log 2>&1
+32 17 * * * python3 ilm.py article >> ilm_Log 2>&1
+12 18 * * * python3 ilm.py article >> ilm_Log 2>&1
+03 19 * * * python3 ilm.py article >> ilm_Log 2>&1
+08 20 * * * python3 ilm.py article >> ilm_Log 2>&1
 `
 
 ## Configuration
 1. To add a new website make a new variable containing a tuple under "#Tuples of websites and their regular expressions" with the website's URL in the first position and the best way to identify article URLs using regular expression in the second position.
 2. Next add your website's tuple to the WEBlist list under "Lists of tuples".
+3. Add your respective RSS links in order to tweet an episode when it is published.
--- ilm.py
+++ ilm.py
@@ -1,29 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+
 import urllib.request
 import urllib.parse
 import datetime
+import requests
+import twython
+import hashlib
 import random
-import tweepy
 import time
 import bs4
+import sys
 import re
-
-#Tuples of websites and their regular expressions
-Website1 = ('http://www.scientificsaudi.com/main/articles', 'http://www.scientificsaudi.com/ss/[0-9].*')
-Website2 = ('https://syr-res.com', 'https://syr-res.com/article/[0-9].*?.html')
-Website3 = ('http://n-scientific.org', 'http://n-scientific.org/[0-9].*#')
-Website4 = ('https://oloom.news', 'https://oloom\.news/%.*')
-Website5 = ('http://www.bbc.com/arabic/scienceandtech', '/arabic/science-and-tech-[0-9].*')
-Website6 = ('http://www.reuters.com/news/science', '/article/')
-
-#Lists of tuples: websites and their regular expressions
-WEBlist = [Website1, Website2, Website3, Website4, Website5, Website6]
-
-#Twitter credentials
-consumer_key		= ''
-consumer_secret		= ''
-access_token		= ''
-access_token_secret	= ''
+import os
 
 #Terminal text colours
 Black 	= '\x1b[30m'
@@ -36,24 +24,52 @@ Cyan	= '\x1b[36m'
 White	= '\x1b[37m'
 Cancel	= '\x1b[0m'
 
-#Connecting to twitter
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+#Twitter credentials
+key		= ''
+secret		= ''
+token		= ''
+token_secret	= ''
 
-def GetLink(TheWeb, TheRE):
+#Instagram credentials
+OAuth		= ''
+insta = 'https://api.instagram.com/v1/users/self/media/recent/?access_token={}'.format(OAuth)
+
+#Connecting to twitter
+api = twython.Twython(key, secret, token, token_secret)
+
+def md5(words):
+	'''hashes a string'''
+	hash_md5 = hashlib.md5()
+	hash_md5.update(words.encode())
+	return(hash_md5.hexdigest())
+
+def Article():
 	'''
-	Parses a website, gets all links that satisfy the given regular
-	expression with their titles, then returns a random tuple with
-	a URL link and the page's title
+	Parses a website, gets all links that satisfy the given
+	regular expression with their titles, then returns a random
+	tuple with a URL link and the page's title
 	'''
-	web = urllib.request.urlopen(TheWeb)
+	#Tuples of websites and their regular expressions
+	Website1 = ('http://www.scientificsaudi.com/main/articles', 'http://www.scientificsaudi.com/ss/[0-9].*')
+	Website2 = ('https://syr-res.com', 'https://syr-res.com/article/[0-9].*?.html')
+	Website3 = ('http://n-scientific.org', 'http://n-scientific.org/[0-9].*#')
+	Website4 = ('https://oloom.news', 'https://oloom\.news/%.*')
+	Website5 = ('http://www.bbc.com/arabic/scienceandtech', '/arabic/science-and-tech-[0-9].*')
+	Website6 = ('https://ara.reuters.com/news/internet', '/article/')
+	#Lists of tuples: websites and their regular expressions
+	WEBlist = [	Website1, Website2, Website3,
+			Website4, Website5, Website6	]
+	#Random choice
+	random.shuffle(WEBlist)
+	TheWeb = WEBlist[0]
+	#Get article info
+	web = urllib.request.urlopen(TheWeb[0])
 	data = bs4.BeautifulSoup(web, 'lxml')
 	URLs = set()
 	for link in data.find_all('a'):
-		ArticleLinks = link.get('href')
-		if re.search(TheRE, str(ArticleLinks)):
-			URLs.add(urllib.parse.urljoin(TheWeb, ArticleLinks))
+		ArL = link.get('href')
+		if re.search(TheWeb[1], str(ArL)):
+			URLs.add(urllib.parse.urljoin(TheWeb[0], ArL))
 	URL = URLs.pop()
 	article = urllib.request.urlopen(URL)
 	articledata = bs4.BeautifulSoup(article, 'lxml')
@@ -62,34 +78,90 @@ def GetLink(TheWeb, TheRE):
 		Title = Title.replace('–', '-')
 	if re.search('Ř', Title):
 		raise Exception('incorrect title encoding')
-	else:	
+	else:
 		return(Title, URL)
 
-def Tweet(TheWeb, TheRE):
-	'''
-	Tweets a weblink and its title
-	'''
-	Result = GetLink(TheWeb, TheRE)
-	TheTweet = '{}\n{}\n#علوم'.format(Result[0], Result[1])
-	api.update_status(TheTweet)
+def RSS():
+	'''Generates an episode tweet from RSS links'''
+	#SoundCloud RSS
+	SCurl = 'http://www.mstdfr.com/feed/?podcast=ilm'
+	SCrss = requests.get(SCurl)
+	SCdata = bs4.BeautifulSoup(SCrss.content, features='xml')
+	SCitems = SCdata.findAll('item')
+	SCitem = SCitems[0]
+	Numb = SCitem.title.text.split(':')[0]
+	Title = SCitem.title.text.split(':')[1]
+	SndCld = SCitem.link.text
+	#YouTube RSS
+	YTurl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC_iBvLK04bFOzu3nZtZKePA'
+	YTrss = requests.get(YTurl)
+	YTdata = bs4.BeautifulSoup(YTrss.content, features='xml')
+	YTID = YTdata.findAll('yt:videoId')[0].text
+	YouTub = 'https://www.youtube.com/watch?v={}'.format(YTID)
+	Users = 'اذا تبغوا تسجلوا لنا خرجة الحلقة:\nilm.fm/out'
+	#Instagram media
+#	IGurl = 'https://web.stagram.com/rss/n/ilm_fm'
+#	IGurl = 'https://www.instagram.com/ilm_fm/'
+#	IG = urllib.request.urlopen(IGurl)
+#	IGdata = bs4.BeautifulSoup(IG, 'lxml')
+#	IGitems = SCdata.findAll('script')
+#	Media = 'X'
+#	print(IGitems)
+	TEXT = 'حلقة {} {}\n\n#SoundCloud #iTunes\n{}\n\n#YouTube\n{}\n\n{}'.format(Numb, Title, SndCld, YouTub, Users)
+	return(TEXT)
+
+def Tweet(choice):
+	'''Tweet'''
+	if choice == 'article':
+		text = Article()
+		TheTweet = '{}\n{}\n#علوم'.format(text[0], text[1])
+#		api.update_status(TheTweet)
+		return(True)
+	elif choice == 'rss':
+		TheTweet = RSS()
+		#Do not repeat tweets
+		if os.path.exists('.check') == False:
+			cf = open('.check', 'w')
+			cf.write('None')
+			cf.close()
+		check1 = md5(TheTweet)
+		cf = open('.check', 'r')
+		for line in cf:
+			check2 = line
+		cf.close()
+		if check1 != check2:
+			cf = open('.check', 'w')
+			cf.write(check1)
+			cf.close()
+#			api.update_status(TheTweet)
+			return(True)
+		else:
+			return(False)
 
 def main():
-	'''
-	Run the script with a loop to by pass errors
-	'''
-	for attempts in range(1000):
-		random.shuffle(WEBlist)
-		TheWebsite = WEBlist[0]
+	'''Run the script with a loop to by pass errors'''
+	for attempts in range(10):
 		dateSTR = datetime.datetime.now().strftime('%d %B %Y @ %H:%M')
 		try:
-			Tweet(TheWebsite[0], TheWebsite[1])
-			print('{}[+] Tweeted -{}{}{}'.format(Green,
-				dateSTR, Cancel, TheWebsite[0]))
-			break
+			tweeted = Tweet(sys.argv[1])
+			if tweeted == True:
+				print('{}[+] Tweeted - {}{}'
+				.format(Green, dateSTR, Cancel))
+				break
+			elif tweeted == False:
+				print('{}[-] Repeated Tweet - {}{}'
+				.format(Red, dateSTR, Cancel))
+				break
 		except Exception as TheError:
-			print('{}[-]{}{}{}{}'.format(Red,
-				str(TheError), dateSTR, Cancel, TheWebsite[0]))
+			print('{}[-]{}{}{}'
+			.format(Red, str(TheError), dateSTR, Cancel))
 			continue
 
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__': main()
+
+
+rss = requests.get(insta).content.decode()
+#data = bs4.BeautifulSoup(rss)
+
+
+print(rss)
